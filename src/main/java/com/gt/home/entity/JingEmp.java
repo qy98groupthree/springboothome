7a7578bfd76096dcb1c86212200434a2f15b3a59
jingmeishi <1346788294@qq.com>
2019-07-26 15:38:48 +0800

    新建了一个类，测试一下

--- src/main/java/com/gt/home/entity/JingEmp.java
+++ src/main/java/com/gt/home/entity/JingEmp.java
@@ -1,15 +1,18 @@
 package com.gt.home.entity;
 
 public class JingEmp {
-    private  Integer id;
-    private  Integer emp_id;
-    private  String emp_name;
-    private  Integer emp_phone;
-    private Integer emp_account;
-    private  Integer emp_password;
-    private  String emp_email;
-    private  Integer emp_pp;
-    private  String emp_headportait;
-    private  String singature;
-    private  Integer emp_staus;
+    public class kl {
+        private  Integer id;
+        private  Integer emp_id;
+        private  String emp_name;
+        private  Integer emp_phone;
+        private Integer emp_account;
+        private  Integer emp_password;
+        private  String emp_email;
+        private  Integer emp_pp;
+        private  String emp_headportait;
+        private  String singature;
+        private  Integer emp_staus;
+    }
+
 }
