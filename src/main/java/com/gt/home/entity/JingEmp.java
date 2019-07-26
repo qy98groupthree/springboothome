280ed544e8535e340da0cd8cbe4ba31e3f33d4b2
jingmeishi <1346788294@qq.com>
2019-07-26 14:34:47 +0800

    Merge branch 'master' of E:\二期项目\springboothome with conflicts.

---
+++ src/main/java/com/gt/home/entity/JingEmp.java
@@ -0,0 +1,15 @@
+package com.gt.home.entity;
+
+public class JingEmp {
+    private  Integer id;
+    private  Integer emp_id;
+    private  String emp_name;
+    private  Integer emp_phone;
+    private Integer emp_account;
+    private  Integer emp_password;
+    private  String emp_email;
+    private  Integer emp_pp;
+    private  String emp_headportait;
+    private  String singature;
+    private  Integer emp_staus;
+}
